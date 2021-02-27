3823b93acf444ea0590f7e65f145f6b96ce6737d
Duncan Armstrong <duncan.armstrong@gmail.com>
2021-02-27 13:45:02 -0800

    Removed password parameter altogether

--- scripts/start-testkitchen.sh
+++ scripts/start-testkitchen.sh
@@ -24,7 +24,6 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-#./valheim_server.x86_64 -name "My server" -port 2456 -world "Dedicated" -password "secret"
-./valheim_server.x86_64 -name "Lunch Crew Alumni (Testkitchen)" -password "password" -nographics -batchmode -port 2456 -world "testkitchen" -savedir "/opt/valheim-server/config" -public 1
+./valheim_server.x86_64 -name "Lunch Crew Alumni (Testkitchen)" -nographics -batchmode -port 2456 -world "testkitchen" -savedir "/opt/valheim-server/config" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
